0d53e8d9606085f854807f9e07b55e6c96836fe1
Mohd Numan <48924562+one-numan@users.noreply.github.com>
2023-05-21 18:47:48 +0530

    Add Code |  sort_Alph_order

---
+++ String_Question/sort_Alph_order.py
@@ -0,0 +1,14 @@
+"""Program to sort alphabetically
+ the words from a String provide by user """
+#  using Traditional Approch
+
+
+in_word='How are You Brother'
+word=in_word.split(" ")
+for i in word:
+    print(i[::-1],end=" ")
+    pass
+
+
+
+
